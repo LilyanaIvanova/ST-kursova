550b4dfd743215e9f77b1d9a22af56849243c3aa
Andrian Dimitrov <134285460+AndrianDimitrov@users.noreply.github.com>
2023-11-10 20:10:43 +0200

    Update change-role.js
    Update

--- commands/moderation/change-role.js
+++ commands/moderation/change-role.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, Permissions } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,7 +6,7 @@ module.exports = {
     .setDescription("Changes role for a member in the server")
     .addUserOption(option =>
       option.setName("user")
-        .setDescription("Select the user")
+        .setDescription("Select the member")
         .setRequired(true))
     .addRoleOption(option =>
       option.setName('role')
@@ -17,16 +17,34 @@ module.exports = {
         .setDescription("Add or remove the role")
         .setRequired(true)),
   async execute(interaction) {
+    if (!interaction.member.permissions.has(Permissions.FLAGS.MANAGE_ROLES)) {
+      await interaction.reply("You don't have permission to manage roles.");
+      return;
+    }
+
     const user = interaction.options.getMember('user');
     const role = interaction.options.getRole('role');
     const addRole = interaction.options.getBoolean('add');
 
+    if (!interaction.guild.me.roles.highest.comparePositionTo(role) > 0) {
+      await interaction.reply("Don't have permission to manage this role.");
+      return;
+    }
+
     if (user && role) {
       try {
         if (addRole) {
+          if (user.roles.cache.has(role.id)) {
+            await interaction.reply(`${user.user.tag} already has the "${role.name}" role.`);
+            return;
+          }
           await user.roles.add(role);
           await interaction.reply(`Added the role "${role.name}" to ${user.user.tag}.`);
         } else {
+          if (!user.roles.cache.has(role.id)) {
+            await interaction.reply(`${user.user.tag} does not have the "${role.name}" role.`);
+            return;
+          }
           await user.roles.remove(role);
           await interaction.reply(`Removed the role "${role.name}" from ${user.user.tag}.`);
         }

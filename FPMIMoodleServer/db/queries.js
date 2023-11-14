a17e368c5af98f310809e2321390ce043d8c2e0e
Ivan Valkov <vanko1vii@gmail.com>
2023-11-14 21:26:26 +0200

    Transfering excel data to database

--- FPMIMoodleServer/db/queries.js
+++ FPMIMoodleServer/db/queries.js
@@ -2,8 +2,46 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
   user: 'admin',
-  host: 'localhost', // FIXME: Temp
+  host: 'localhost', 
   database: 'student_data',
   password: 'admin',
   port: 5432,
 })
+const XLSX = require('xlsx');
+const pgp = require('pg-promise')();
+
+const dbConfig = {
+  host: 'localhost',
+  port: '5432',
+  database: 'uni_info',
+  user: 'postgres',
+  password: 'g@lin123', 
+};
+
+
+const db = pgp(dbConfig);
+
+
+const workbook = XLSX.readFileSync('C:/Users/User/Downloads/RawStudentData.xlsx');
+const sheetName = 'Simple';
+const worksheet = workbook.Sheets[sheetName];
+
+
+const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 }).filter(row => row.some(cell => cell !== null));
+
+
+const tableName = 'student_info';
+const columns = ['mail', 'oks', 'course', 'fac', 'fac_num', 'grp', 'full_name', 'spec', 'status'];
+const insertQuery = pgp.helpers.insert(data, columns, { table: tableName });
+
+
+
+console.log(insertQuery);
+db.none(insertQuery)
+  .then(() => {
+    console.log('Data inserted successfully!');
+    pgp.end();
+  })
+  .catch(error => {
+    console.error('Error inserting data:', error);
+  });
